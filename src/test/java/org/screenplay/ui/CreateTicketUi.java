0f1c37237437ce31ba67041b18c3fc76f370599e
Alejandra Marín <marinrinconalejandra@gmail.com>
2026-03-11 23:29:32 -0500

    refactor: remove dead code (TheUser, TicketListUi and unused constants in CreateTicketUi)

--- src/test/java/org/screenplay/ui/CreateTicketUi.java
+++ src/test/java/org/screenplay/ui/CreateTicketUi.java
@@ -5,9 +5,6 @@ import org.openqa.selenium.By;
 
 public class CreateTicketUi {
 
-    public static final Target PAGE_TITLE =
-            Target.the("título de la página crear ticket").locatedBy(".create-ticket-title");
-
     public static final Target TITLE_FIELD =
             Target.the("campo título del ticket").locatedBy("#ticket-title");
 
@@ -17,9 +14,6 @@ public class CreateTicketUi {
     public static final Target SUBMIT_BUTTON =
             Target.the("botón enviar ticket")
                   .located(By.cssSelector("button.form-button[type='submit']"));
-
-    public static final Target ERROR_ALERT =
-            Target.the("alerta de error en formulario de ticket").locatedBy(".error-alert");
 
     private CreateTicketUi() {
     }
